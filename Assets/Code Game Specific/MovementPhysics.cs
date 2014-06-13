8cef41959ac7bca4fb55c8a125c5db64791e9811
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-06-13 23:49:04 +0200

    Initial ship movement and camera control

---
+++ Assets/Code Game Specific/MovementPhysics.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovementPhysics : MonoBehaviour 
+{
+    
+    public float Speed;
+    public float TurningSpeed;
+    private Rigidbody rigid;
+    private Vector3 addedVelocity;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigid = this.rigidbody;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        rigid.velocity += addedVelocity * Time.fixedDeltaTime;
+        Debug.Log(rigid.velocity);
+        addedVelocity = Vector3.zero;
+	}
+
+    public void Move(Vector2 dir)
+    {
+        addedVelocity = dir.normalized * Speed;
+        addedVelocity.z = addedVelocity.y;
+        addedVelocity.y = 0;
+    }
+
+    public void LookAt(Vector3 point)
+    {
+
+    }
+
+}
---
+++ Assets/Code Game Specific/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using XboxCtrlrInput;
+
+[RequireComponent(typeof(MovementPhysics))]
+public class PlayerController : MonoBehaviour 
+{
+    private MovementPhysics movPhysics;
+
+	// Use this for initialization
+	void Start () 
+    {
+        movPhysics = this.GetComponent<MovementPhysics>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector2 movInput = new Vector2(XCI.GetAxis(XboxAxis.LeftStickX), XCI.GetAxis(XboxAxis.LeftStickY));
+        movPhysics.Move(movInput);
+        Debug.Log(movInput);
+	}
+}
