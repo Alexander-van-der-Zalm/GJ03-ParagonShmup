404070b687d180dda0ce873d880f9f05626e86e3
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-06-14 09:42:53 +0200

    Test

--- Assets/Code Game Specific/MovementPhysics.cs
+++ Assets/Code Game Specific/MovementPhysics.cs
@@ -7,6 +7,9 @@ public class MovementPhysics : MonoBehaviour
     
     public float Speed;
     public float TurningSpeed;
+    public float FloatingHeight;
+    public float FloatingFalloff;
+
     private Rigidbody rigid;
     private Vector3 addedVelocity;
 
@@ -19,10 +22,16 @@ public class MovementPhysics : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate () 
     {
+        addVelocity();
+        maintainFloat();
+	}
+
+    private void addVelocity()
+    {
         rigid.velocity += addedVelocity * Time.fixedDeltaTime;
         Debug.Log(rigid.velocity);
         addedVelocity = Vector3.zero;
-	}
+    }
 
     public void Move(Vector2 dir)
     {
